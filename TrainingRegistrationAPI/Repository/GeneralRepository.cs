d5a1f9fcb5264b13a106818d1a34165b2ef36839
Chairul Hamid <chairulhamid01@gmail.com>
2021-11-21 15:36:23 +0700

    commit-launchSetting

--- TrainingRegistrationAPI/Repository/GeneralRepository.cs
+++ TrainingRegistrationAPI/Repository/GeneralRepository.cs
@@ -10,8 +10,8 @@ using TrainingRegistrationAPI.Repository.Interface;
 namespace TrainingRegistrationAPI.Repository
 {
     public class GeneralRepository<Context, Entity, Key> : IRepository<Entity, Key>
-         where Entity : class
-         where Context : MyContext
+        where Entity : class
+        where Context : MyContext
     {
         private readonly MyContext myContext;
         private readonly DbSet<Entity> entities;
@@ -24,7 +24,10 @@ namespace TrainingRegistrationAPI.Repository
 
         public int Delete(Key key)
         {
-            throw new NotImplementedException();
+            var item = entities.Find(key);
+            entities.Remove(item);
+            var result = myContext.SaveChanges();
+            return result;
         }
 
         public IEnumerable<Entity> Get()
@@ -34,17 +37,27 @@ namespace TrainingRegistrationAPI.Repository
 
         public Entity Get(Key key)
         {
-            throw new NotImplementedException();
+            return entities.Find(key); //menggunakan Find untuk mencari Primary Key 
         }
 
         public int Insert(Entity entity)
         {
+            entities.Add(entity);
+            var result = myContext.SaveChanges();
+            return result;
+        }
+
+        public int Register(RegisterVM entity)
+        {
             throw new NotImplementedException();
         }
 
+
         public int Update(Entity entity, Key key)
         {
-            throw new NotImplementedException();
+            myContext.Entry(entity).State = EntityState.Modified;
+            var result = myContext.SaveChanges();
+            return result;
         }
     }
 }
