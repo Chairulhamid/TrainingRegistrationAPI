1b195d7e5ef185736ae33da5661da940f9bc593d
sharonathasya <92560714+sharonathasya@users.noreply.github.com>
2021-11-21 21:54:57 +0700

    commit Register Employee

--- TrainingRegistrationAPI/Controllers/EmployeesController.cs
+++ TrainingRegistrationAPI/Controllers/EmployeesController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using TrainingRegistrationAPI.Context;
 using TrainingRegistrationAPI.Controller.Base;
 using TrainingRegistrationAPI.Models;
 using TrainingRegistrationAPI.Repository.Data;
+using TrainingRegistrationAPI.ViewModel;
 
 namespace TrainingRegistrationAPI.Controllers
 {
@@ -14,8 +18,64 @@ namespace TrainingRegistrationAPI.Controllers
     [ApiController]
     public class EmployeesController : BaseController<Employee, EmployeeRepository, int>
     {
-        public EmployeesController(EmployeeRepository employeeRepository) : base(employeeRepository)
+        private readonly EmployeeRepository employeeRepository;
+        //private readonly MyContext myContext;
+        public IConfiguration _configuration;
+        public EmployeesController(EmployeeRepository employeeRepository, IConfiguration configuration, MyContext myContext) : base(employeeRepository)
         {
+            this.employeeRepository = employeeRepository;
+            //this.myContext = myContext; 
+            this._configuration = configuration;
+        }
+
+        /*[Authorize(Roles = "Admin")]*/
+        //[EnableCors("AllowOrigin")]
+        [Route("Profile")]
+        [HttpGet]
+        public ActionResult<RegisterEmpVM> GetProfile()
+        {
+            var result = employeeRepository.GetProfile();
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return NotFound(new { status = HttpStatusCode.NotFound, result = result, message = "Tidak ada data di tabel" });
+
+        }
+
+        /*[Authorize(Roles = "employee, Admin")]*/
+        //[Route("CariProfile/{nik}")]
+        [HttpGet("Profile/{nik}")]
+        public ActionResult GetProfile(string Email)
+        {
+
+            var result = employeeRepository.GetProfile(Email);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return NotFound(new { status = HttpStatusCode.NotFound, result = result, message = "Data dengan Email tersebut tidak ditemukan" });
+
+        }
+
+        [Route("RegisterEmp")]
+        [HttpPost]
+        public ActionResult RegisterEmp(RegisterEmpVM registerEmpVM)
+        {
+            var check = employeeRepository.RegisterEmp(registerEmpVM);
+            if (check == 1)
+            {
+                return Ok(new { status = HttpStatusCode.OK, message = "Data berhasil ditambahkan" });
+            }
+            if (check == 2)
+            {
+                return BadRequest(new { status = HttpStatusCode.BadRequest, message = "Data gagal ditambahkan. Email sudah terdaftar" });
+            }
+            else
+            {
+                return BadRequest(new { status = HttpStatusCode.BadRequest, message = "Data gagal ditambahkan. Phone sudah terdaftar" });
+
+            }
 
         }
     }
--- TrainingRegistrationAPI/Startup.cs
+++ TrainingRegistrationAPI/Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -7,9 +8,11 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using TrainingRegistrationAPI.Context;
 using TrainingRegistrationAPI.Repository.Data;
@@ -45,6 +48,27 @@ namespace TrainingRegistrationAPI
             {
                 c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             });
+
+            services.AddAuthentication(auth =>
+            {
+                auth.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                auth.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = true;
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = false,
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"])),
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero
+                };
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -59,6 +83,7 @@ namespace TrainingRegistrationAPI
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
