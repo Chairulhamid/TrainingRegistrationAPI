6ce87ad820170188a014f08d68f9545545e60b02
Chairul Hamid <chairulhamid01@gmail.com>
2021-11-24 20:34:58 +0700

    commit-modul

--- TrainingRegistrationAPI/Controllers/ModulsController.cs
+++ TrainingRegistrationAPI/Controllers/ModulsController.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using TrainingRegistrationAPI.Context;
 using TrainingRegistrationAPI.Controller.Base;
 using TrainingRegistrationAPI.Models;
 using TrainingRegistrationAPI.Repository.Data;
+using TrainingRegistrationAPI.ViewModel;
 
 namespace TrainingRegistrationAPI.Controllers.Base
 {
@@ -14,9 +19,32 @@ namespace TrainingRegistrationAPI.Controllers.Base
     [ApiController]
     public class ModulsController : BaseController<Modul, ModulRepository, int>
     {
-        public ModulsController(ModulRepository modulRepository) : base(modulRepository)
+        private readonly ModulRepository modulRepository;
+        private readonly MyContext myContext;
+        public IConfiguration _configuration;
+        public ModulsController(ModulRepository modulRepository, IConfiguration configuration, MyContext myContext) : base(modulRepository)
         {
-
+            this.modulRepository = modulRepository;
+            this.myContext = myContext;
+            this._configuration = configuration;
+        }
+        [Route("RegisterModul")]
+        [HttpPost]
+        public ActionResult RegisterModul(ModulVM modulVM)
+        {
+            var check = modulRepository.RegisterModul(modulVM);
+            if (check == 1)
+            {
+                return Ok(new { status = HttpStatusCode.OK, message = "Data berhasil ditambahkan" });
+            }
+            if (check == 2)
+            {
+                return BadRequest(new { status = HttpStatusCode.BadRequest, message = "Data gagal ditambahkan. Modul sudah terdaftar" });
+            }
+            else
+            {
+                return BadRequest(new { status = HttpStatusCode.BadRequest, message = "Data gagal ditambahkan." });
+            }
         }
     }
 }
--- TrainingRegistrationAPI/Repository/Data/ModulRepository.cs
+++ TrainingRegistrationAPI/Repository/Data/ModulRepository.cs
@@ -4,13 +4,34 @@ using System.Linq;
 using System.Threading.Tasks;
 using TrainingRegistrationAPI.Context;
 using TrainingRegistrationAPI.Models;
+using TrainingRegistrationAPI.ViewModel;
 
 namespace TrainingRegistrationAPI.Repository.Data
 {
     public class ModulRepository : GeneralRepository<MyContext, Modul, int>
     {
+        private readonly MyContext myContext;
         public ModulRepository(MyContext myContext) : base(myContext)
         {
+            this.myContext = myContext;
+        }
+        public int RegisterModul(ModulVM modulVM)
+        {
+            Modul modul = new Modul();
+            var checkModulTitle = myContext.Moduls.Where(x => x.ModulTittle == modulVM.ModulTitle).FirstOrDefault();
+            modul.ModulTittle = modulVM.ModulTitle;
+
+            if (checkModulTitle != null)
+            {
+                return 2;
+            }
+            modul.ModulTittle = modulVM.ModulTitle;
+            modul.ModulDesc = modulVM.ModulDesc;
+            modul.ModulContent = modulVM.ModulContent;
+            modul.CourseId = modulVM.CourseId;
+            myContext.Moduls.Add(modul);
+            var result = myContext.SaveChanges();
+            return result;
         }
     }
 }
