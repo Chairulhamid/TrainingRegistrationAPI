aecdaff133bb8e0026c635164f442cb6b0063170
ADZF <92560775+ADZF@users.noreply.github.com>
2021-11-24 14:55:30 +0700

    commit topic repo

--- TrainingRegistrationAPI/Controllers/TopicsController.cs
+++ TrainingRegistrationAPI/Controllers/TopicsController.cs
@@ -1,21 +1,56 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using API.ViewModel;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using TrainingRegistrationAPI.Context;
 using TrainingRegistrationAPI.Controller.Base;
 using TrainingRegistrationAPI.Models;
 using TrainingRegistrationAPI.Repository.Data;
+using TrainingRegistrationAPI.ViewModel;
 
-namespace TrainingRegistrationAPI.Controllers.Base
+namespace TrainingRegistrationAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class TopicsController : BaseController<Topic, TopicRepository, int>
     {
-        public TopicsController(TopicRepository topicRepository) : base(topicRepository)
+        private readonly TopicRepository topicRepository;
+        private readonly MyContext myContext;
+        public IConfiguration _configuration;
+        public TopicsController(TopicRepository topicRepository, IConfiguration configuration, MyContext myContext) : base(topicRepository)
         {
+            this.topicRepository = topicRepository;
+            this.myContext = myContext;
+            this._configuration = configuration;
+        }
+
+        [Route("RegisterTopic")]
+        [HttpPost]
+        public ActionResult RegisterTopic(TopicVM topicVM)
+        {
+            var check = topicRepository.RegisterTopic(topicVM);
+            if (check == 1)
+            {
+                return Ok(new { status = HttpStatusCode.OK, message = "Data berhasil ditambahkan" });
+            }
+            if (check == 2)
+            {
+                return BadRequest(new { status = HttpStatusCode.BadRequest, message = "Data gagal ditambahkan. Topic sudah terdaftar" });
+            }
+            else
+            {
+                return BadRequest(new { status = HttpStatusCode.BadRequest, message = "Data gagal ditambahkan. Topic sudah terdaftar" });
+
+            }
 
         }
     }
---
+++ TrainingRegistrationAPI/ViewModel/TopicVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrainingRegistrationAPI.ViewModel
+{
+    public class TopicVM
+    {
+        [Required]
+        public string TopicName { get; set; }
+        [Required]
+        public string TopicDesc{ get; set;}
+    }
+}
