7e68a3db48382dd46126506b565f4b1d8423496d
Chairul Hamid <chairulhamid01@gmail.com>
2021-11-25 17:57:20 +0700

    commit-Payment

--- TrainingRegistrationAPI/Repository/Data/PaymentRepository.cs
+++ TrainingRegistrationAPI/Repository/Data/PaymentRepository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using TrainingRegistrationAPI.Context;
 using TrainingRegistrationAPI.Models;
+using TrainingRegistrationAPI.ViewModel;
 
 namespace TrainingRegistrationAPI.Repository.Data
 {
@@ -14,10 +15,30 @@ namespace TrainingRegistrationAPI.Repository.Data
         {
             this.myContext = myContext;
         }
-
-       /* public int DataPayment(Payment payment)
+        //REGISTER
+        public int RegisterPay(PaymentVM paymentVM)
         {
+            Payment payment= new Payment();
+        //REGISTERED COURSE
+            RegisteredCourse registeredCourse= new RegisteredCourse();
+            registeredCourse.UserId= paymentVM.UserId;
+            registeredCourse.CourseId= paymentVM.CourseId;
+            myContext.RegisteredCourses.Add(registeredCourse);
+            myContext.SaveChanges();
+            
+            //SAVE PAYMENT
+            payment.RegisteredCourseId = registeredCourse.RegisteredCourseId;
+            payment.PaymentDate = paymentVM.PaymentDate;
+            payment.BankAccount = paymentVM.BankAccount;
+            payment.TotalPayment = paymentVM.TotalPayment;
+            payment.Status = 0 ;
+            myContext.Payments.Add(payment);
+            var result = myContext.SaveChanges();
+            return result;
+        }
+        /* public int DataPayment(Payment payment)
+         {
 
-        }*/
+         }*/
     }
 }
