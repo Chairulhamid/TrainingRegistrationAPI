3948c7172983f558487f805dfec8c16a246157db
Chairul Hamid <chairulhamid01@gmail.com>
2021-11-28 13:59:07 +0700

    commit

--- TrainingRegistrationClient/wwwroot/assets/js/LayoutSubUser.js
+++ TrainingRegistrationClient/wwwroot/assets/js/LayoutSubUser.js
@@ -87,21 +87,30 @@ function getDCourse(courseId) {
         } //End of AJAX error function  
     });
 }
-function Random() {
-  
+function maketextnumber(n) {
+    for (var r = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"], e = n, t = new Array, a = 0; a <= e - 1; a++) {
+        t[a] = r[parseInt(Math.random() * r.length)];
+        t = t;
+        randomtextnumber = t.join("")
+    }
+   
 }
+var hasil = (maketextnumber(3), randomtextnumber);
+
 function getPayCourse(courseId) {
     console.log(courseId)
-            var acak =5;
     $.ajax({
         type: "GET",
         url: "https://localhost:44307/API/Courses/GetApvdIdCourse/" + courseId,
         contentType: "application/json; charset=utf-8",
         dataType: "json",
         success: function (data) {
-            console.log(data[0].courseFee);
+            const numb = data[0].courseFee;
+            const format = numb.toString().split('').reverse().join('');
+            const convert = format.match(/\d{1,3}/g);
+            const rupiah = 'Rp ' + convert.join('.').split('').reverse().join('').substr(0,3)
             $("#courseName").html(data[0].courseName);
-            $("#payment").html(data[0].courseFee + acak);
+            $("#payment").html(rupiah + hasil);
         }, //End of AJAX Success function  
 
         failure: function (data) {
@@ -112,81 +121,3 @@ function getPayCourse(courseId) {
         } //End of AJAX error function  
     });
 }
-
-
-/*$(document).ready(function () {
-    $.ajax({
-        type: "GET",
-        url: "https://localhost:44307/API/Courses",
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function (data) {
-            console.log(data[0].topic.topicName)
-            //alert(JSON.stringify(data));                  
-            $("#DIV").html('');
-            var rows = '';
-            $.each(data, function (i, item) {
-                rows = `
-                               <div class="col-xl-3 col-md-6 d-flex align-items-stretch mb-3" data-aos="zoom-in" data-aos-delay="100">
-                        <div class="product">
-                            <div class="imgbox"> <img src="${item.courseImg}"> </div>
-                            <div class="specifies">
-                                <h2>${item.courseName} <br> <span> ${item.topic.topicName}</span></h2>
-                                <div class="price" >Rp. ${item.courseFee}</div> <label>Description</label>
-                                <p>${item.courseDesc}</p>
-                                <a href="user/DetailCourse/${item.courseId}" id="klik_menu"   class="btn btn-warning klik_menu" id="buyCourse" /*onclick="getDCourse('${item.courseId}')">Buy Now</a>
-                            </div>
-                        </div>
-                    </div>
-                            `;
-                $('#lesson').append(rows);
-            }); //End of foreach Loop   
-            console.log(data);
-        }, //End of AJAX Success function  
-
-        failure: function (data) {
-            alert(data.responseText);
-        }, //End of AJAX failure function  
-        error: function (data) {
-            alert(data.responseText);
-        } //End of AJAX error function  
-    });
-});
-
-$(document).ready(function(){
-    getDCourse(dataID);
-        });
-function getDCourse(courseId) {
-    console.log(courseId)
-    $.ajax({
-        type: "GET",
-        url: "https://localhost:44307/API/Courses/GetApvdIdCourse/" + courseId,
-        contentType: "application/json; charset=utf-8",
-        dataType: "json",
-        success: function (data) {
-            console.log(data)
-            *//*window.location.replace = "https://localhost:44344/auth/User";*//*
-            //window.location.assign("https://localhost:44344/User/DetailCourse")
-            //alert(JSON.stringify(data));                  
-            $("#DIV").html('');
-            var rows = '';
-            $.each(data, function (i, item) {
-                rows = `
-                             <div class="course-info d-flex justify-content-between align-items-center">
-                            <h5>Trainer</h5>
-                            <p><a href="#">Walter White</a></p>
-                        </div>
-                            `;
-                $('#materiDetail').html();
-            }); //End of foreach Loop   
-            console.log(data);
-        }, //End of AJAX Success function  
-
-        failure: function (data) {
-            alert(data.responseText);
-        }, //End of AJAX failure function  
-        error: function (data) {
-            alert(data.responseText);
-        } //End of AJAX error function  
-    });
-}*/
