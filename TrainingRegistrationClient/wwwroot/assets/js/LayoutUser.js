b533b587a163070f2520fd152cc898999bb7012d
sharonathasya <92560714+sharonathasya@users.noreply.github.com>
2021-11-29 10:00:16 +0700

    committ

--- TrainingRegistrationClient/wwwroot/assets/js/LayoutUser.js
+++ TrainingRegistrationClient/wwwroot/assets/js/LayoutUser.js
@@ -51,44 +51,4 @@ $(document).ready(function () {
     });
 });
 
-function InsertDataEmpIndex() {
-    var today = new Date();
-    var dd = String(today.getDate()).padStart(2, '0');
-    var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-    var yyyy = today.getFullYear();
-    today = mm + '/' + dd + '/' + yyyy;
 
-    var obj = new Object();
-    obj.FirstName = $('#firstName').val();
-    obj.LastName = $('#lastName').val();
-    obj.Email = $('#email').val();
-    obj.Phone = $('#phone').val();
-    obj.Gender = $('#gender').val();
-    obj.CV = $('#cv').val();
-    obj.Address = $('#address').val();
-    obj.BirthDate = $('#birthDate').val();
-    obj.password = $('#password').val();
-    obj.RegistDate = today;
-    obj.RoleId = 3;
-    console.log(obj);
-    $.ajax({
-        url: "/Employees/RegisterEmp",
-        'type': 'POST',
-        'data': { entity: obj }, //objek kalian
-        'dataType': 'json',
-    }).done((result) => {
-        Swal.fire({
-            icon: 'success',
-            title: 'Success!',
-            text: 'Register Successful!'
-        }).then(function () {
-            window.location = "https://localhost:44344/Home/LoginEmp";
-        });
-    }).fail((error) => {
-        Swal.fire({
-            icon: 'error',
-            title: 'Oops..',
-            text: 'Failed to Add Data'
-        });
-    });
-};
